63a8e7871e7e15400d9c5a32dcbc8e8565882e11
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2024-04-13 11:48:50 -0700

    import necessary libs

---
+++ handwritten-digit-recognition/predict_digits.py
@@ -0,0 +1,8 @@
+# Pyplot
+import matplotlib.pyplot as plt
+
+# datasets, classifiers and performance metrics.
+from sklearn import datasets, metrics, svm
+from sklearn.model_selection import train_test_split
+
+print("Hello sci-kit learn!")
